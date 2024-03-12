0bf88f8dd0a4fa3ec2c378a5572e7b1c894fcbcc
Yasmeen <yomalbalooshi@gmail.com>
2024-03-12 14:45:50 +0300

    sign in form

--- src/components/SignIn.vue
+++ src/components/SignIn.vue
@@ -1,10 +1,54 @@
 <script>
 export default {
-  name: 'SignIn'
+  name: 'SignIn',
+  data: () => ({
+    email: '',
+    password: '',
+    rules: [
+      (value) => {
+        if (value) return true
+
+        return 'Fill Out Field'
+      }
+    ]
+  })
 }
 </script>
 <template>
-  <section>
-    <h1>Sign In Page</h1>
+  <section class="mt-16">
+    <v-card class="mx-auto pa-10" max-width="700">
+      <p class="text-h4 mb-10 text-md-center">Sign In</p>
+
+      <v-form @submit.prevent>
+        <v-text-field
+          class="mb-8"
+          label="Email"
+          variant="outlined"
+          type="email"
+          :rules="rules"
+        ></v-text-field>
+        <v-text-field
+          class="mb-8"
+          label="Password"
+          variant="outlined"
+          type="password"
+          :rules="rules"
+        ></v-text-field>
+        <v-btn
+          type="submit"
+          color="red-darken-4"
+          rounded="xl"
+          size="x-large"
+          class="mt-2 mb-8"
+          block
+        >
+          Sign In
+        </v-btn>
+      </v-form>
+      <p>
+        Not a Student Yet?
+        <router-link to="/registerStudent">Apply Here</router-link>
+      </p>
+    </v-card>
   </section>
 </template>
---
+++ src/components/students/RegisterStudent.vue
@@ -0,0 +1,10 @@
+<script>
+export default {
+  name: 'RegisterStudent'
+}
+</script>
+<template>
+  <section>
+    <h1>Register</h1>
+  </section>
+</template>
